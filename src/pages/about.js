32da2857bb62039193b7115d50cf3c1064ab0e44
unknown <stiv.bajro@gmail.com>
2022-03-09 14:29:02 +0100

    changes

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>
+        <h1>About page</h1>
+        <h2>Child protection and inclusion</h2>
+        <p>Every child has the right to grow up in a safe and inclusive environment
+            UNICEF works with partners around the world to promote policies and expand access to services that protect all children.</p>
+        <h2>Child survival</h2>
+        <p>Every child has the right to survive and thrive
+            UNICEF has helped reduce child mortality all over the world by working to reach the most vulnerable children, everywhere.</p>
+        <h2>Education</h2>
+        <p>Every child has the right to learn
+            UNICEF works around the world to support quality learning for every girl and boy, especially those in greatest danger of being left behind.</p>
+        <h2>Social policy</h2>
+        <p>Every child has the right to an equitable chance in life
+            UNICEF works around the world to reduce child poverty and shield girls and boys from its lifelong consequences.</p>
+    </div>
+  )
+}
